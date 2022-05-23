cf68aaa4a823be7130c1debea64ca5d0bec05b86
Thauan Amorim <thauanssussu@gmail.com>
2022-05-23 20:43:21 -0300

    base estrutural

---
+++ src/main/java/com/observatorio_pedagogico/observatorio_pedagogico/ObservatorioPedagogicoApplication.java
@@ -0,0 +1,13 @@
+package com.observatorio_pedagogico.observatorio_pedagogico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservatorioPedagogicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservatorioPedagogicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/observatorio_pedagogico/observatorio_pedagogico/ObservatorioPedagogicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.observatorio_pedagogico.observatorio_pedagogico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservatorioPedagogicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
