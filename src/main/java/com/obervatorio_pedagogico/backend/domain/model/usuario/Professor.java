0d130d71efc6e5a210c282cb01a014eaf96c55ec
0jafc0 <jafclive0@gmail.com>
2022-05-30 20:25:38 -0300

    criado model do funcionarioCoped e Professor

---
+++ src/main/java/com/obervatorio_pedagogico/backend/domain/model/usuario/Professor.java
@@ -0,0 +1,17 @@
+package com.obervatorio_pedagogico.backend.domain.model.usuario;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "t_professor")
+public class Professor extends Usuario {
+    
+}
