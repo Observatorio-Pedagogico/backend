eed4f2e23646387939f2ba57113cf0dae63891cb
0jafc0 <jafclive0@gmail.com>
2022-06-02 18:23:37 -0300

    alterado nome da classe EnderecoModel para Endereco

--- src/main/java/com/obervatorio_pedagogico/backend/infrastructure/persistence/repository/endereco/EnderecoRepository.java
+++ src/main/java/com/obervatorio_pedagogico/backend/infrastructure/persistence/repository/endereco/EnderecoRepository.java
@@ -1,11 +1,11 @@
 package com.obervatorio_pedagogico.backend.infrastructure.persistence.repository.endereco;
 
-import com.obervatorio_pedagogico.backend.domain.model.endereco.EnderecoModel;
+import com.obervatorio_pedagogico.backend.domain.model.endereco.Endereco;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
     
 }
