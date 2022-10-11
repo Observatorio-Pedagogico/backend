78c187e0f99c861d6272870522813c0919bc3fc5
Lucas <lukas.lopes@klok.tech>
2022-10-11 18:54:31 -0300

    ajuste no controller de Funcionario

--- src/main/java/com/obervatorio_pedagogico/backend/application/controllers/funcionario/FuncionarioController.java
+++ src/main/java/com/obervatorio_pedagogico/backend/application/controllers/funcionario/FuncionarioController.java
@@ -1,22 +1,15 @@
 package com.obervatorio_pedagogico.backend.application.controllers.funcionario;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.obervatorio_pedagogico.backend.application.services.usuario.FuncionarioService;
 import com.obervatorio_pedagogico.backend.domain.exceptions.NaoEncontradoException;
 import com.obervatorio_pedagogico.backend.infrastructure.utils.httpResponse.ResponseService;
 import com.obervatorio_pedagogico.backend.infrastructure.utils.modelMapper.ModelMapperService;
-import com.obervatorio_pedagogico.backend.presentation.dto.usuario.request.FuncionarioRequest;
 import com.obervatorio_pedagogico.backend.presentation.dto.usuario.response.FuncionarioResponse;
 import com.obervatorio_pedagogico.backend.presentation.model.usuario.EnvelopeFuncionario;
 import com.obervatorio_pedagogico.backend.presentation.shared.Response;
-
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/observatorio-pedagogico/api/funcionario")
@@ -31,8 +24,8 @@ public class FuncionarioController {
     private ModelMapperService modelMapperService;
     
     @GetMapping("/token")
-    public ResponseEntity<Response<FuncionarioResponse>> getTodos(@RequestBody FuncionarioRequest funcionarioRequest){
-        EnvelopeFuncionario envelopeFuncionario = funcionarioService.buscarFuncionarioPorToken(funcionarioRequest.getToken())
+    public ResponseEntity<Response<FuncionarioResponse>> getTodos(@RequestHeader("token") String token) {
+        EnvelopeFuncionario envelopeFuncionario = funcionarioService.buscarFuncionarioPorToken(token)
             .orElseThrow(() -> new NaoEncontradoException());
 
         return responseService.ok(modelMapperService.convert(envelopeFuncionario.getFuncionario(), FuncionarioResponse.class));
