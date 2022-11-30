21dea917cf233df76a8ddabbce58bf67dc73f92a
Thauan Amorim <thauanssussu@gmail.com>
2022-11-29 21:35:48 -0300

    Update nome da variavel

--- src/main/java/com/obervatorio_pedagogico/backend/infrastructure/persistence/repository/disciplina/DisciplinaRepository.java
+++ src/main/java/com/obervatorio_pedagogico/backend/infrastructure/persistence/repository/disciplina/DisciplinaRepository.java
@@ -22,6 +22,6 @@ public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>, Q
     @Query(value = "select * from t_disciplina disciplina where disciplina.codigo in :codigos", nativeQuery = true)
     public List<Disciplina> findDisciplinaByCodigo(@Param("codigos") List<String> codigos);
 
-    @Query(value = "select distinct on (td.codigo) td.* from t_disciplina td", nativeQuery = true)
+    @Query(value = "select distinct on (disciplina.codigo) disciplina.* from t_disciplina disciplina", nativeQuery = true)
     public List<Disciplina> findDisciplinaIgnorePeriodo();
 }
